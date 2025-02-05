e1c497e58b066e752d2d6556c8740a74bcccd27e
unknown <yxd21mtuuea.ac.uk>
2025-02-05 00:02:05 +0000

    reverted back to railway database url

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-#DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:xnSUjUJMOYqAXilgEkAbrPvnWHCxwkwZ@postgres.railway.internal:5432/railway"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
